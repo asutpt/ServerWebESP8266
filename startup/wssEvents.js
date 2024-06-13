8e92bdafc8d13b5066d7326f61fce42d7dbedab6
user <123@>
2024-06-13 23:15:30 +0300

    13_06_20241=>1

--- startup/wssEvents.js
+++ startup/wssEvents.js
@@ -1,9 +1,22 @@
 const cvc = require('../controllers/currentvalueController');
 const concplant = require('../controllers/concplantController');
 
+let count = 0;
 
 const events = (app, aWss) => {
+    let interval = setInterval( () => {
+            // if(count < 10) {
+            //
+            //     console.log(count  + "  =>>  SendBroadCast!");
+            // }
+             count = count + 1;
+            console.log(count);
+            broadcastMessage({Field: "id_master", Value: count}, "Checking");
+        }
+        , 50);
     app.ws('/', (ws) => {
+        console.log("3rf3f ");
+
         //ws.id = Date.now(); //для приватной комнаты
         ws.on('message', (messageWS) => {
             try{
@@ -20,6 +33,7 @@ const events = (app, aWss) => {
                         broadcastMessage(messageWS, ws);
                         break;
                     case 'sendDataSQL':
+                        console.log(messageWS);
                         concplant.create(messageWS, messageWS.Plant).then();
                         broadcastMessage(messageWS, ws);
                         break;
@@ -39,19 +53,26 @@ const events = (app, aWss) => {
 
 
 
-    const broadcastMessage = (message) => {     //отправляем всем сообщение широковещательное
+
+
+    const broadcastMessage = (mes, event) => {     //отправляем всем сообщение широковещательное
+        //console.log(aWss.clients);
         aWss.clients.forEach(client => {
             //if(client.id === id){}        //для приватной комнаты
-            message.username = 'СЕРВЕР';
-            message.id = Date.now();
-            message.event = Date.now();
-            const _message = message.message;
-            message.message = "Нахуй ты мне это прислал??  ";
+            let message = {};
+            message.Id = Date.now();
+            message.Event = event;
+            message.Username = 'Server';
+            message.Plant = 0;
+            message.Message = mes;
+
             client.send(JSON.stringify(message));
         })
     }
 
 }
+
+
 
 
 module.exports = events;
