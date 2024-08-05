17ec359e11d0976a643c76990ba9051ec78abd41
asutpt <asutpt@gmail.com>
2024-08-05 17:10:50 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	controllers/concplantController.js
    #	startup/wssEvents.js
    #	tempate.js

--- state/func.js
+++ state/func.js
@@ -2,6 +2,9 @@ const {parse} = require('date-fns');
 
 
 const parseDate = (date) => {
+    if(date === -1 || date === '-1') {
+        date =  date = new Date('1970-01-01');
+    }
     return parse(date.toString(), 'dd.MM.yyyy HH:mm:ss', new Date());
 }
 
